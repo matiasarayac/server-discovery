941fb30196e5d7c0c5507c5c3f5ba9ebfb41dc80
Araya Cohen <matias.araya.cohen@accenture.com>
2018-04-09 14:46:51 -0300

    modify date windows

--- windows/run-windows.bat
+++ windows/run-windows.bat
@@ -1,5 +1,10 @@
 FOR /F "usebackq" %%i IN (`hostname`) DO SET host=%%i
-For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c%%a%%b)
+
+FOR /F "TOKENS=1* DELIMS= " %%A IN ('DATE/T') DO SET CDATE=%%B
+FOR /F "TOKENS=1,2 eol=/ DELIMS=/ " %%A IN ('DATE/T') DO SET mm=%%B
+FOR /F "TOKENS=1,2 DELIMS=/ eol=/" %%A IN ('echo %CDATE%') DO SET dd=%%B
+FOR /F "TOKENS=2,3 DELIMS=/ " %%A IN ('echo %CDATE%') DO SET yyyy=%%B
+SET mydate=%yyyy%%mm%%dd%
 
 set h=%TIME:~0,2%
 set m=%TIME:~3,2%
